4e399272959a62cfe8128615e8d376e558df8f25
Joel <joeldario9912@gmail.com>
2025-07-23 14:54:20 -0300

    fix: conflict rebase branch develop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import validationSchema from "./schemas/app.schema";
-import { ProfilesModule } from './profiles/profiles.module';
-import { ContactMessagesModule } from './contact-messages/contact-messages.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { InterestedModule } from './interested/interested.module';
-import { GalleriesModule } from './galleries/galleries.module';
-import { AdoptersModule } from './adopters/adopters.module';
-import { NewsModule } from './news/news.module';
-import { UsersModule } from './users/users.module';
+import { ProfilesModule } from "./profiles/profiles.module";
+import { ContactMessagesModule } from "./contact-messages/contact-messages.module";
+import { NotificationsModule } from "./notifications/notifications.module";
+import { InterestedModule } from "./interested/interested.module";
+import { GalleriesModule } from "./galleries/galleries.module";
+import { AdoptersModule } from "./adopters/adopters.module";
+import { NewsModule } from "./news/news.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
   imports: [
