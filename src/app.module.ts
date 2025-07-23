427e5c1c8cc56039a29272a637ccb8ab851a9449
Joel <joeldario9912@gmail.com>
2025-07-23 14:29:17 -0300

    fix: conflict rebase branch develop solved

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import validationSchema from "./schemas/app.schema";
+import { PrismaService } from "./prisma.service";
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, validationSchema })],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
