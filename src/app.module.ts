adbd2c1334fce423b1c1868e33386a74a11c268d
Joel <joeldario9912@gmail.com>
2025-07-23 13:46:45 -0300

    feat: config implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import validationSchema from './schemas/app.schema';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import validationSchema from "./schemas/app.schema";
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, validationSchema })],
