bb39cee1839352deaa7c16a02557fa51f821b428
Joel <joeldario9912@gmail.com>
2025-07-23 01:28:10 -0300

    feat: config setup added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import validationSchema from "./schemas/app.schema";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal: true, validationSchema})],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get("PORT");
+  Logger.log(`Server running on port ${port}`);
+  await app.listen(port);
 }
+
 void bootstrap();
